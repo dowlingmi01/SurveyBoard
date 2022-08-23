d2223dbe06b0bc08435a86d5c55beeb0bae85feb
dowlingmi01 <michaeldowling440@gmail.com>
2022-08-23 10:13:33 -0600

    stylesheet updates

--- config/routes.rb
+++ config/routes.rb
@@ -13,8 +13,4 @@ Rails.application.routes.draw do
   root 'surveys#index'
 
   resources :surveys
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
